1af85dbd6ab4f4be76308e36b058d7c85b633f3b
suris <surista@gmail.com>
2021-01-04 21:09:45 +0900

    update wk2

---
+++ cmake-build-debug/CMakeFiles/dartmouth_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dartmouth_c.dir/main.c.obj"
+  "dartmouth_c.exe"
+  "dartmouth_c.exe.manifest"
+  "dartmouth_c.pdb"
+  "libdartmouth_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dartmouth_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    int leaves;
+    scanf("%d", &height);
+    scanf("%d", &leaves);
+
+    int Tinuviel = (height <= 5 && leaves >= 8);
+    int Calaelen = (height >= 10 && leaves >= 10);
+    int Falarion = (height <= 8 && leaves <= 5);
+    int Dorthonion = (height >= 12 && leaves <= 7);
+
+    if (Tinuviel)
+    {
+        printf("Tinuviel");
+    } else if (Calaelen)
+    {
+        printf("Calaelen");
+    }else if (Falarion)
+    {
+        printf("Falarion");
+    }else if (Dorthonion)
+    {
+        printf("Dorthonion");
+    }
+    else
+    {
+        printf("Uncertain");
+    }
+
+}
